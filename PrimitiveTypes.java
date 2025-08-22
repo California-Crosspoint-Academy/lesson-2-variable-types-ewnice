822c9584b593d7d4333c09a13a5948cb1595131c
ewnice <eunicechan26@crosspointacademy.org>
2025-08-22 16:14:13 +0000

    PrimitiveTypes

---
+++ PrimitiveTypes.java
@@ -0,0 +1,11 @@
+public class PrimitiveTypes {
+    public static void main (String[] args) {
+        String a = "My name is Eunice";
+        int b = -124982;
+        double c = 0.341893421;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
